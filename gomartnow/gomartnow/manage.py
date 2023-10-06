80838abdf4ddf0570c416fa20f89574fbd115ac0
Homesacience98 <itssajj@gmail.com>
2023-10-06 18:51:13 +0100

    Add new featu

--- gomartnow/gomartnow/manage.py
+++ gomartnow/gomartnow/manage.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
+    # Activate the virtual environment (replace '/path/to/your/venv' with your actual virtual environment path)
+    activate_script = '/path/to/your/venv/bin/activate'
+    if os.path.exists(activate_script):
+        os.system(f"source {activate_script}")
+
+    # Set the DJANGO_SETTINGS_MODULE
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gomartnow.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +19,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+    # Run the Django management command (replace '<your_command_here>' with your desired command)
+    execute_from_command_line(['manage.py', '<your_command_here>'])
 
 if __name__ == '__main__':
     main()
