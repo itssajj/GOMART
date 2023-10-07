ae1d576b4d76bd4ad5c28f907e6e23cd70efbea0
Homesacience98 <itssajj@gmail.com>
2023-10-07 08:25:36 +0100

    build

--- gomartnow/gomartnow/manage.py
+++ gomartnow/gomartnow/manage.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+
 def main():
-    # Activate the virtual environment (replace '/path/to/your/venv' with your actual virtual environment path)
-    activate_script = '/path/to/your/venv/bin/activate'
-    if os.path.exists(activate_script):
-        os.system(f"source {activate_script}")
-
-    # Set the DJANGO_SETTINGS_MODULE
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gomartnow.settings')
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,10 +14,10 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
+            
         ) from exc
+    execute_from_command_line(sys.argv)
 
-    # Run the Django management command (replace '<your_command_here>' with your desired command)
-    execute_from_command_line(['manage.py', '<your_command_here>'])
 
 if __name__ == '__main__':
     main()
